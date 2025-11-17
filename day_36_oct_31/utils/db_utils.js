6c35b519a6fc35ed577459f8b0c6e0a316d25f20
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-11-17 08:42:39 +0530

    CRON

--- day_36_oct_31/utils/db_utils.js
+++ day_36_oct_31/utils/db_utils.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDB(dbName = 'nareshit_sept_2025') {
-    mongoose.connect(`mongodb+srv://sanjaysamantra1:berhampur@cluster0.ga81v5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    mongoose.connect(`mongodb+srv://sanjaysamantra1:berhampur@cluster0.ga81v5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/nareshit_sept_2025`)
         .then(() => console.log('Connected To Database!'))
         .catch(err=>{console.log('Error connecting DB')})
 }
