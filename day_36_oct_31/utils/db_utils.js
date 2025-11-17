2df5eb75a7d75bac609f075d2d22ff0f55d5df18
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-11-17 08:24:14 +0530

    changed DB name

--- day_36_oct_31/utils/db_utils.js
+++ day_36_oct_31/utils/db_utils.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDB(dbName = 'nareshit_sept_2025') {
-    mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`)
+    mongoose.connect(`mongodb+srv://sanjaysamantra1:berhampur@cluster0.ga81v5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
         .then(() => console.log('Connected To Database!'))
         .catch(err=>{console.log('Error connecting DB')})
 }
